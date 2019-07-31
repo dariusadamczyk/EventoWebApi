ce1b159625d63405d944211aa07284247dc48f39
darius.adamczyk@outlook.com <darius.adamczyk@outlook.com>
2019-07-31 22:40:21 +0200

    Add JwtHandler

---
+++ Evento.Infrastructure/DTO/JwtDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Evento.InfraStructure.DTO
+{
+    public class JwtDto
+    {
+          public string Token { get; set; }
+          public long Expires { get; set; }
+    }
+}
---
+++ Evento.Infrastructure/Services/IJwtHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Evento.InfraStructure.DTO;
+
+namespace Evento.InfraStructure.Services
+{
+    public interface IJwtHandler
+    {
+        JwtDto Create(Guid userId, string role);
+    }
+}
